7e837f7caf49e9a878e3be6b8791dadba0c9064b
Shamasis Bhattacharya <mail@shamasis.net>
2015-03-18 20:34:23 +0530

    Added integration test sails app

---
+++ tests/integration/api/models/Request.js
@@ -0,0 +1,46 @@
+/**
+ * Request.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/#!documentation/models
+ */
+
+module.exports = {
+  schema: true,
+  autosubscribe: false,
+  attributes: {
+    name: {
+      type: 'string'
+    },
+    user: {
+      model: 'User'
+    },
+    collection: {
+      model: 'Collection'
+    },
+    transactionID: {
+      type: 'string'
+    },
+    updated: {
+      type: 'boolean',
+      defaultsTo: false
+    }
+  },
+
+  createInstance: function (user, collection, cb) {
+    Request.create({
+      name: 'request:' + user.id + ':' + collection.id,
+      user: user
+    }, function (err, request) {
+      if (err) { return cb(err); }
+
+      collection.requests.add(request.id);
+      collection.save(function (err, collection) {
+        if (err) { return cb(err); }
+
+        cb(null, collection);
+      });
+    });
+  }
+};
+
---
+++ tests/integration/api/models/User.js
@@ -0,0 +1,32 @@
+/**
+ * User.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/#!documentation/models
+ */
+
+module.exports = {
+  schema: true,
+  autosubscribe: false,
+  attributes: {
+    name: {
+      type: 'string'
+    },
+    admin: {
+      type: 'boolean',
+      defaultsTo: false
+    },
+    collections: {
+      collection: 'Collection',
+      via: 'user'
+    },
+    teams: {
+      collection: 'Team',
+      via: 'members'
+    },
+    transactionID: {
+      type: 'string'
+    }
+  }
+};
+
