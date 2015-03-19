404f08c1a903ec362e63bf670cd620ed5e98330f
Shamasis Bhattacharya <mail@shamasis.net>
2015-03-20 00:30:02 +0530

    Added script for keeping integration test package updated.

---
+++ scripts/test-integration.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+pushd tests/integration > /dev/null;
+
+if [ ! -d 'node_modules' ]; then
+	npm install;
+else
+	if [ -d 'node_modules/sails-mysql-transactions' ]; then
+		rm -rf 'node_modules/sails-mysql-transactions';
+	fi
+	npm install ../../;
+fi
+
+popd > /dev/null;
