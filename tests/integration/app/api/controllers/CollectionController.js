570947b6a03368e0e84705f70072e33fca36a9f3
Ankit Sobti <ankit@getpostman.com>
2015-10-16 16:47:17 +0530

    Collection controller with find method

--- tests/integration/app/api/controllers/CollectionController.js
+++ tests/integration/app/api/controllers/CollectionController.js
@@ -6,6 +6,14 @@
  */
 
 module.exports = {
-	
+  find: function (req, res) {
+    Collection.findOne(req.param('id')).populate('requests').exec(function (err, collection) {
+      if (err) {
+        return res.serverError(err);
+      }
+
+      return res.ok(collection);
+    })
+  }
 };
 
